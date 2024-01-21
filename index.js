e45b67ae8410c6863ea0eeab40ae2d192b84b3bb
ravi mishra <ravimishrafea.560722@gmail.com>
2024-01-22 00:44:21 +0530

    done responsive part

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    var dropdown = document.querySelector('.dropdown');
+  var dropdown = document.querySelector('.dropdown2');
 
-    dropdown.addEventListener('mouseover', function() {
-      document.querySelector('.dropdown-content').style.display = 'block';
-    });
-
-    dropdown.addEventListener('mouseout', function() {
-      document.querySelector('.dropdown-content').style.display = 'none';
-    });
+  dropdown.addEventListener('mouseover', function() {
+    document.querySelector('.dropdown-content').style.display = 'block';
   });
+
+  dropdown.addEventListener('mouseout', function() {
+    document.querySelector('.dropdown-content').style.display = 'none';
+  });
+});
+
+
+
+      function toggleDropdown() {
+          var dropdown = document.getElementById("dropdown");
+          if (dropdown.style.display === "block") {
+              dropdown.style.display = "none";
+          } else {
+              dropdown.style.display = "block";
+          }
+      }
+
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function (event) {
+          if (!event.target.matches('.hamburger-icon')) {
+              var dropdown = document.getElementById("dropdown");
+              if (dropdown.style.display === "block") {
+                  dropdown.style.display = "none";
+              }
+          }
+      };
